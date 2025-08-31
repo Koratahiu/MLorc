2506e721b98d43e481bcb37c0a98e691a61b5a20
Koratahiu <haidermontage191@gmail.com>
2025-08-31 18:34:37 +0300

    fix init

--- MLorc_optim/__init__.py
+++ MLorc_optim/__init__.py
@@ -1,6 +1,19 @@
-from .optim.MLorc_AdamW import MLorc_AdamW
-from .optim.MLorc_Prodigy import MLorc_Prodigy
-from .optim.MLorc_Lion import MLorc_Lion
-from .optim.MLorc_DAdapt_Lion import MLorc_DAdapt_Lion
-from .optim.MLorc_Adopt import MLorc_Adopt
-from .optim.MLorc_CAME import MLorc_CAME
+from .optim import (
+    MLorc_AdamW,
+    MLorc_Adopt,
+    MLorc_CAME,
+    MLorc_DAdapt_Lion,
+    MLorc_Lion,
+    MLorc_Prodigy,
+)
+
+__all__ = [
+    "MLorc_AdamW",
+    "MLorc_Adopt",
+    "MLorc_CAME",
+    "MLorc_DAdapt_Lion",
+    "MLorc_Lion",
+    "MLorc_Prodigy",
+]
+
+__version__ = "0.1.7"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="MLorc_optim",
-    version="0.1.6",
+    version="0.1.7",
     author="Koratahiu",
     author_email="hiuhonor@gmail.com",
     license='Apache 2.0',
